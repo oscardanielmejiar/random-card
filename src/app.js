8218b69a0e9716be8c52e1595005f6c3cd6149fa
oscardanielmejiar <oscardanielmejiar@gmail.com>
2022-09-22 00:56:58 +0000

    almost done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generarteValue() {
+  let numberList = [
+    "K",
+    "Q",
+    "J",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10"
+  ];
+  let valueNumber = Math.floor(Math.random() * numberList.length);
+  return valueNumber;
+}
+function generarteSuit() {
+  let suitList = ["	&#9824;", "	&#9829;", "	&#9827;", "&#9830;"];
+  let suitType = Math.floor(Math.random() * suitList.length);
+  return suitType;
+}
