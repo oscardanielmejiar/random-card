288c3982d0ce000f5a6765d52e5a0cfe938a69fe
oscardanielmejiar <oscardanielmejiar@gmail.com>
2022-09-23 17:05:15 +0000

    finally

--- src/app.js
+++ src/app.js
@@ -2,27 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-function generarteValue() {
-  let numberList = [
-    "K",
-    "Q",
-    "J",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10"
-  ];
-  let valueNumber = Math.floor(Math.random() * numberList.length);
-  return valueNumber;
+window.onload = function() {
+  randomSuit();
+};
+
+let numarray = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let suitArr = ["&#9829;", "&#9830;", "&#9824;", "&#9827;"];
+
+function getValue(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function generarteSuit() {
-  let suitList = ["	&#9824;", "	&#9829;", "	&#9827;", "&#9830;"];
-  let suitType = Math.floor(Math.random() * suitList.length);
-  return suitType;
+
+let cardNumber = document.querySelector(".card-value");
+let topSuit = document.querySelector(".top-suit");
+let bottomSuit = document.querySelector(".bottom-suit");
+cardNumber.innerHTML = numarray[getValue(0, numarray.length - 1)];
+
+function randomSuit() {
+  let suit = suitArr[getValue(0, suitArr.length - 1)];
+  topSuit.innerHTML = suit;
+  bottomSuit.innerHTML = suit;
 }
